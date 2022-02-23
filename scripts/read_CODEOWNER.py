667972a2df43c84b48f731ff4deedafed784957d
Ty Ricard <tricard@uvic.ca>
2022-02-22 16:21:37 -0800

    Improve on Python Script

--- scripts/read_CODEOWNER.py
+++ scripts/read_CODEOWNER.py
@@ -1,8 +1,23 @@
 import sys
+import re
+# The following source was used for the projectL
+# [1] Python Contributors. "Regular expression operations." docs.python.org.
+#     Available: https://docs.python.org/3/library/re.html
 
 def main():
     args = sys.argv[1:]
-    print(args)
+    output = []
+
+    for file_name in args:
+        f = open(file_name, "r")
+        pattern = re.compile(file_name)
+        lines = f.readlines()
+
+        for l in lines:
+            if pattern.match(l) is not None:
+                output.append(file_name)
+                break
+    return output
 
 if __name__ == "__main__":
     main()
