7216e0075760c40d184a3cb19d46f2aaea7ce2fb
Ty Ricard <tricard@uvic.ca>
2022-02-22 19:38:45 -0800

    This is a test

--- python_actions/read_CODEOWNERS.py
+++ python_actions/read_CODEOWNERS.py
@@ -1,29 +1,36 @@
 import sys
 import re
-import json
+import os
 # The following source was used for the projectL
 # [1] Python Contributors. "Regular expression operations." docs.python.org.
 #     Available: https://docs.python.org/3/library/re.html
+# 
+# [2] https://able.bio/rhett/how-to-set-and-get-environment-variables-in-python--274rgt5
 #
-# [2] GeekForGeek Contributors. "How To Convert Python Dictionary To JSON?"
-#     GeeksForGeeks.org. Available: https://www.geeksforgeeks.org/how-to-convert-python-dictionary-to-json/
 
 def main():
-    args = sys.argv[1:]
-    output = {"files": []}
+    output = []
+    file_str = os.getenv('INPUT_FILES')
+    files = file_str.split('|')
+    print(file_str)
+    
     r = open(".github/CODEOWNERS", "r")
 
-    for file_name in args:
+    for file_name in files:
         pattern = re.compile(file_name)
         lines = r.readlines()
 
         for l in lines:
             if pattern.match(l) is not None:
-                output["files"].append(file_name)
+                output.append(file_name)
                 break
      
     r.close()
-    return json.dumps(output, indent = 4) 
+    ret_str = "The required strings are the following: \n"
+    for file_name in output:
+        ret_str = ret_str + file_name + "\n"
+    
+    return ret_str
 
 if __name__ == "__main__":
     main()
