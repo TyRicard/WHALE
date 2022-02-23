34f4fb225e775b40013135e5e08bd4ce10dd658c
Ty Ricard <tricard@uvic.ca>
2022-02-22 17:54:37 -0800

    Test

--- python_actions/read_CODEOWNERS.py
+++ python_actions/read_CODEOWNERS.py
@@ -1,14 +1,17 @@
 import sys
 import re
+import json
 # The following source was used for the projectL
 # [1] Python Contributors. "Regular expression operations." docs.python.org.
 #     Available: https://docs.python.org/3/library/re.html
+#
+# [2] GeekForGeek Contributors. "How To Convert Python Dictionary To JSON?"
+#     GeeksForGeeks.org. Available: https://www.geeksforgeeks.org/how-to-convert-python-dictionary-to-json/
 
 def main():
     args = sys.argv[1:]
-    output = []
+    output = {"files": []}
     r = open(".github/CODEOWNERS", "r")
-    w = open(".github/NEED_UPDATE_CODEOWNER_FILES", "w")
 
     for file_name in args:
         pattern = re.compile(file_name)
@@ -16,13 +19,11 @@ def main():
 
         for l in lines:
             if pattern.match(l) is not None:
-                output.append(file_name)
+                output["files"].append(file_name)
                 break
-
-    w.write(output)           
+     
     r.close()
-    w.close()
-    return
+    return json.dumps(output, indent = 4) 
 
 if __name__ == "__main__":
     main()
